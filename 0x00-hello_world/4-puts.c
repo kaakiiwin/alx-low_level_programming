a85e05bb45cf5ce1bb75f53e9ba3acf4105614e6
kaakiiwin <kaakiiteam@gmail.com>
2022-12-12 06:39:26 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,12 @@
- #include <stdio.h>                                                                                                                      
+#include <stdio.h>
 
-/**                                                                                                                                     
+/**
+ * main - Entry
  *
- *  * main - Entry                                                                                                                         
- *
- *   * Return: Always 0                                                                                                                     
- *
- *    */                                                                                                                                     
+ *  Return: Always 0 (Success)                                                      */                                                                                
 
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-	        puts("\"Programming is like building a multi lingual puzzle);                                                            
-
-			        return (0);
-
+int main(void)
+{
+	puts("\"Programming is like building a multi lingual puzzle");
+	return (0);
 }
